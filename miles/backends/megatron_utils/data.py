2ddd192b606310eb600d8a9297dac2e36e3258a9
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-11-17 22:32:34 +0800

    Super tiny refactor wandb step computation (#189)

--- miles/backends/megatron_utils/data.py
+++ miles/backends/megatron_utils/data.py
@@ -12,7 +12,7 @@ from megatron.core.packed_seq_params import PackedSeqParams
 from miles.utils import train_metric_utils
 from miles.utils.data import get_minimum_num_micro_batch_size
 from miles.utils.flops_utils import calculate_fwd_flops
-from miles.utils.metric_utils import compute_pass_rate
+from miles.utils.metric_utils import compute_pass_rate, compute_rollout_step
 from miles.utils.seqlen_balancing import get_seqlen_balanced_partitions
 from miles.utils.types import RolloutBatch
 
@@ -119,11 +119,7 @@ def gather_log_data(
         logger.info(f"{metric_name} {rollout_id}: {reduced_log_dict}")
 
         # Calculate step once to avoid duplication
-        step = (
-            rollout_id
-            if not args.wandb_always_use_train_step
-            else rollout_id * args.rollout_batch_size * args.n_samples_per_prompt // args.global_batch_size
-        )
+        step = compute_rollout_step(args, rollout_id)
         reduced_log_dict["rollout/step"] = step
         tracking_utils.log(args, reduced_log_dict, step_key="rollout/step")
 
